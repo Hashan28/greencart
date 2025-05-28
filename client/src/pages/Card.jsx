1b49670c22dd00dd54c2d5524897c0067597b197
Hashan28 <pasindudhanawardana@gmail.com>
2025-05-28 14:11:34 +0530

    Complete project

--- client/src/pages/Card.jsx
+++ client/src/pages/Card.jsx
@@ -65,6 +65,23 @@ const Card = () => {
                 }else{
                     toast.error(data.message)
                 }
+            }else{
+                // place order with stripe 
+                  const {data}= await axios.post('/api/order/stripe', {
+                    userId: user._id,
+                    items: cartArray.map(item=> ({
+                        product: item._id, 
+                        quantity: item.quantity || 1
+                    })),
+                    address: selectedAddress._id
+                })
+
+                if (data.success) {
+                   window.location.replace(data.url)
+                }else{
+                    toast.error(data.message)
+                }
+
             }
         } catch (error) {
              toast.error(error.message)
